a3198b7a553089e7a7d17a467725d754ff23c49c
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2023-03-19 16:05:14 -0300

    create withdraw route

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - [x] Deve ser possível criar uma conta
 - [x] Deve ser possível buscar o extrato bancário do cliente
 - [x] Deve ser possível realizar um depósito
-- [ ] Deve ser possível realizar um saque
+- [x] Deve ser possível realizar um saque
 - [ ] Deve ser possível buscar o extrato bancário do cliente por data
 - [ ] Deve ser possível atualizar dadis da conta do cliente
 - [ ] Deve ser possível obter dados da conta do cliente
@@ -16,7 +16,7 @@
 
 - [x] Não deve ser possível cadastrar uam conta com CPF já existente
 - [x] Não deve ser possível fazer depósito numa conta não existente
-- [ ] Não deve ser possível buscar extrato numa conta não existente
-- [ ] Não deve ser possível fazer saque numa conta não existemte
+- [x] Não deve ser possível buscar extrato numa conta não existente
+- [x] Não deve ser possível fazer saque numa conta não existemte
 - [ ] Não deve ser possível excluir uma conta não existente
-- [ ] Não deve ser possível fazer saque quanro o saldo for insuficiente
+- [x] Não deve ser possível fazer saque quanro o saldo for insuficiente
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const { v4: uuid } = require("uuid")
+const {response} = require("express");
 const app = express()
 const port = 3030
 const customers = []
@@ -15,6 +16,18 @@ function verifyIfExistAccountCPF(request, response, next) {
     }
     request.customer = customer
     return next()
+}
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === "credit") {
+            return acc + operation.amount
+        } else {
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
 }
 app.post("/account",
     (request, response) => {
@@ -51,6 +64,26 @@ app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
         amount,
         created_at: new Date(),
         type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
+
+app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
+    const { amount } = request.body
+    const { customer } = request
+    const balance = getBalance(customer.statement)
+
+    if(balance < amount) {
+        return response.status(400).json({error: "No funds!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
     }
 
     customer.statement.push(statementOperation)
