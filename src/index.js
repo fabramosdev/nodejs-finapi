2a71e6d9b333e08b0055467f87a9e3e7da008db3
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2023-03-19 16:28:05 -0300

    get statement by date

--- src/index.js
+++ src/index.js
@@ -91,6 +91,15 @@ app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
     return response.status(201).send()
 })
 
+app.get("/statement/date", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request
+    const { date } = request.query
+    const dateFormat = new Date(date + " 00:00")
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+    return response.json(statement)
+})
+
 app.listen(port, () => {
     console.log(`Api started on port ${port}`)
 })
